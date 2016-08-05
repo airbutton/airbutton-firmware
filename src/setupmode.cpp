ef55e1c190362b6511cac7140348e74b305f9ece
gallochri <chri@gallochri.com>
2016-08-05 07:46:38 +0200

    Cleaning the code

--- src/setupmode.cpp
+++ src/setupmode.cpp
@@ -1,42 +1,32 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <WiFiClient.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
+#include "setupmode.h"
 
-#include <ColorBlink.h>
-#include "config.h"
-#include "utils.h"
-#include "webserver.h"
-
-void setupMode() {
-    digitalWrite(RETPIN, HIGH);
+void setupMode(Adafruit_NeoPixel *led, ColorBlink *blinkLed){
+    digitalWrite(RETPIN,HIGH);
     Serial.println("Setup mode started");
     setupModeStatus = true;
     DNSServer DNS_SERVER;
 
     //WiFI start in client mode
-    blinkLed.violet(&led, 100, 1);
+    blinkLed->violet(led, 100, 1);
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
-    delay(100);
+    delay(200);
 
     //Write SSID_LIST in html
-    blinkLed.violet(&led, 100, 1);
+    blinkLed->violet(led, 100, 1);
     Serial.println("Scan WiFi networks");
     SSID_LIST = ssidList();
     delay(100);
 
     //WiFi start in access point mode
-    blinkLed.violet(&led, 100, 1);
+    blinkLed->violet(led, 100, 1);
     WiFi.mode(WIFI_AP);
     WiFi.softAPConfig(AP_IP, AP_IP, IPAddress(255, 255, 255, 0));
     WiFi.softAP(AP_SSID);
     DNS_SERVER.start(53, "*", AP_IP);
     Serial.print("Starting Access Point at ");
     Serial.println(WiFi.softAPIP());
-    blinkLed.violet(&led, 100, 1);
+    blinkLed->violet(led, 100, 2);
 
     //TODO
     // Settings Page
---
+++ src/setupmode.h
@@ -0,0 +1,25 @@
+#ifndef SETUPMODE_H
+#define SETUPMODE_H
+
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
+#include <Adafruit_NeoPixel.h>
+#include <ColorBlink.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WebServer.h>
+#include "utils.h"
+#include "webserver.h"
+
+extern uint8_t RETPIN;
+extern boolean setupModeStatus;
+extern IPAddress AP_IP;
+extern const char *AP_SSID;
+extern unsigned long startTime;
+extern String SSID_LIST;
+extern ESP8266WebServer WEB_SERVER;
+
+void setupMode(Adafruit_NeoPixel *led,
+               ColorBlink *blinkLed);
+
+#endif // SETUPMODE_H
