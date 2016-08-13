bf0b7649a7a12582fe7bd63b8a86f57307380252
teopost <teopost@gmail.com>
2016-08-13 23:51:21 +0200

    Removed IFTTT comment (added for debugging purpose)

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ void setup(){
         return;
     }
     //Button is connected! try to call to IFTTT
-    /*
+
     for (int i = 0; i < 3; i++) {
         if (ifttt()) {
             Serial.println("Success ifttt!");
@@ -51,7 +51,7 @@ void setup(){
             blinkLed.red(&led, 100, 2);
         }
     }
-*/
+
     //Button is connected! try to call to IFTTT
     for (int i = 0; i < 3; i++) {
         if (customurl()) {
